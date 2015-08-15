c5d14c3dcd55747e66b11d506324255f2ff7ac68
Szilveszter Szilagyi <szilveszter@gmx.co.uk>
2015-08-15 07:23:50 +0100

    help menu, better user interaction (getch, include termios, move away from ANSI)

--- anki.c
+++ anki.c
@@ -8,6 +8,60 @@
 #include <time.h>
 //#include <regex.h>
 
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new = old; /* make new settings same as old settings */
+  new.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
+/* Let's test it out */
+//int main(void) {
+//  char c;
+//  printf("(getche example) please type a letter: ");
+//  c = getche();
+//  printf("\nYou typed: %c\n", c);
+//  printf("(getch example) please type a letter...");
+//  c = getch();
+//  printf("\nYou typed: %c\n", c);
+//  return 0;
+//}
 char* curtime()
 
 {
@@ -88,17 +142,30 @@ struct lesson {
     //char card_back[128];
     char *card_front;
     char *card_back;
-    char *score;
+    char *experience;
     int seen;
 };
 
 typedef int (*compfn)(const void*, const void*);
 
+int verbose;
 char nextchar;
 
-int main(void)
+int main(int argc, char *argv[])
 
 {
+    for (int i=0;i<argc;i++) {
+        if(strcmp(argv[i], "-v")==0)
+            verbose = 1;
+        if(strcmp(argv[i], "-h")==0 || strcmp(argv[i], "--help")==0) {
+            printf("\nanki clone in your console");
+            printf("\n\nOptions");
+            printf("\n -h, --help    show help");
+            printf("\n -v            increase verbosity");
+            printf("\n");
+            return 0;
+        }
+    }
     int lines_allocated = 128;
     int max_line_len = 500000;
 
@@ -127,7 +194,7 @@ int main(void)
 
         lessons[i].card_front = malloc(128);
         lessons[i].card_back = malloc(128);
-        lessons[i].score = malloc(32);
+        lessons[i].experience = malloc(32);
 
         /*
         //printf("%i %i %i\n",i,lines_allocated);
@@ -171,7 +238,7 @@ int main(void)
                 j = -1;
             }
             else if(column==0) lessons[i].date[j] = nextchar;
-            else if(column==1) lessons[i].score[j] = nextchar;
+            else if(column==1) lessons[i].experience[j] = nextchar;
             else if(column==2) lessons[i].card_front[j] = nextchar;
             else if(column==3) lessons[i].card_back[j] = nextchar;
         }
@@ -182,14 +249,14 @@ int main(void)
 
     int j;
     for(j = 0; j < i; j++)
-        printf("date: %s, score: %i, front: %s ----- back: %s\n", lessons[j].date, atoi(lessons[j].score), lessons[j].card_front, lessons[j].card_back);
+        printf("date: %s, experience: %i, front: %s ----- back: %s\n", lessons[j].date, atoi(lessons[j].experience), lessons[j].card_front, lessons[j].card_back);
 
     int compare(struct lesson *elem1, struct lesson *elem2)
     {
-        if (atoi(elem1->score) < atoi(elem2->score))
+        if (atoi(elem1->experience) < atoi(elem2->experience))
             return -1;
 
-        else if (atoi(elem1->score) > atoi(elem2->score))
+        else if (atoi(elem1->experience) > atoi(elem2->experience))
             return 1;
 
         else
@@ -205,19 +272,44 @@ int main(void)
          );
 
     for(j = 0; j < 10; j++) {
-        printf("date: %s, score: %i, front: %s\n", lessons[j].date, atoi(lessons[j].score), lessons[j].card_front);
-        int a;
-        printf("Press any key to show the solution: ");
-        getch();
-        printf("solution is: %s\n", lessons[j].card_back);
-        printf("press you score 0-worst, 5-best: ");
-        scanf("%d", &a);
-        printf("You entered: %d\n", a);
-        a = -1;
+        printf("\n\n%i) date: %s, experience: %i, front: %s", j+1, lessons[j].date, atoi(lessons[j].experience), lessons[j].card_front);
+        char *feels_know, *did_know;
+        feels_know = malloc(1);
+        did_know = malloc(1);
+
+        int f, d, lower, higher;
+        printf("\n Do you know the solution? Enter a number between 0 and 5, (0)-no idea, (5)-know it perfectly: ");
+        while(f==0) {
+            f = atoi(feels_know);
+            *feels_know = getche();
+        }
+        printf("\n The solution is: %s", lessons[j].card_back);
+        printf("\n Did you know the solution? Enter a number between 0 and 5, (0)-I missed it, (5)-I knew it: ");
+        *did_know = getche();
+        d = atoi(did_know);
+        if(f>d) {lower = d; higher = f;}
+        else {lower = f; higher = d;}
+        //if(f == 5 || d == 5) {
+            //printf("\n Experience points increased by (%i-%i+1)*(5-%i)=%i", higher, lower, d, (higher-lower+1)*(5-d));
+            printf("\n Experience points increased by %i", d);
+            sprintf(lessons[j].experience, "%i", atoi(lessons[j].experience) + d);
+            printf("%i", *lessons[j].experience);
+        //}
+    }
+
+    FILE *fpw = fopen("ankidbw.txt", "w");
+    if (fpw == NULL)
+    {
+        fprintf(stderr,"Error opening file.\n");
+        exit(2);
+    }
+    else {
+        for(j = 0; j < i; j++)
+            fprintf(fpw, "%s;%i;%s;%s\n", lessons[j].date, atoi(lessons[j].experience), lessons[j].card_front, lessons[j].card_back);
     }
 
     char *curr_time = curtime();
-    printf("%s\n", curr_time);
+    printf("\n %s", curr_time);
 
     struct tm time1 = {0};
 
